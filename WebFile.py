83fc2cae3487a85a10b6fe7f48d162b7706f55f1
kaiki nattan dos santos guedes <156141207+kaikinattandossantos@users.noreply.github.com>
2025-01-22 23:44:46 -0300

    Mudando o método HTTP

--- WebFile.py
+++ WebFile.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", method=["POST"])
 def inicial():
     return "Olá Web"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
