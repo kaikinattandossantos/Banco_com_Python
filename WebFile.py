0d3467e40356c914fcb11ede4aa5cad1e88f8879
kaiki nattan dos santos guedes <156141207+kaikinattandossantos@users.noreply.github.com>
2025-01-22 23:50:12 -0300

    Adicionando uma página web
    Retirado método POST e coloquei uma página chamada "contatos" usando variável

--- WebFile.py
+++ WebFile.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
+@app.route("/")
 def inicial():
     return "Olá Web"
 
+@app.route("/contatos/<nome>")
+def página_de_contatos(nome):
+    return f"<h1> Seu contato: {nome}<h1>" 
 
 if __name__ == '__main__':
     app.run(debug=True)
